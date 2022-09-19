d572d4aea28393315360dde6532f4a7a10527cb8
vitorio-antunes <vitorioantunes01@gmail.com>
2022-09-19 09:35:25 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * -> Tipos primitivos
+ * -> Variáveis no Javascript
+ * -> Operadores matemáticos do Javascript
+ */
+
+// serve para escrever mensagens no console
+
+// string -> textos
+// number -> números
+// boolean -> verdadeiro/falso
+
+console.log('Olá mundo')
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+// variáveis
+let mensagem = 'SoulCode BC 25 JFS' // declaração da variável
+
+console.log(mensagem) // SoulCode BC 25 JFS
+
+mensagem = 'Novo texto'
+
+console.log(mensagem) // Novo texto
